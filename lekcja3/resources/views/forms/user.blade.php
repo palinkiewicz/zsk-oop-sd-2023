250ec95750a7f0c80949688884179e25c1f863d3
palinkiewicz <pogromca.ap@gmail.com>
2023-10-25 09:11:05 +0300

    Create user form view

---
+++ lekcja3/resources/views/forms/user.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>User Form</title>
+</head>
+<body>
+    <form action="" method="get">
+        <input type="text" name="firstName" placeholder="Provide first name">
+        <input type="text" name="lastName" placeholder="Provide last name">
+        <input type="text" name="city" placeholder="Provide city">
+        <input type="submit" value="Submit data">
+    </form>
+</body>
+</html>
