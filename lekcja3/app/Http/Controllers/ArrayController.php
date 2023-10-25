ae8289f465d30fc7bc331984a38a8ba5947ebf9a
palinkiewicz <pogromca.ap@gmail.com>
2023-10-25 08:39:20 +0300

    Create array controller show route

--- lekcja3/app/Http/Controllers/ArrayController.php
+++ lekcja3/app/Http/Controllers/ArrayController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class ArrayController extends Controller
 {
     public function show() {
-        return "ArrayController";
+        return 'ArrayController';
     }
 }
--- lekcja3/routes/web.php
+++ lekcja3/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\ArrayController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,3 +27,5 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::get('show_array', [ArrayController::class, 'show']);
