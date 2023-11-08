8b1ba323997849237b8e2a307d8e266536b89fcb
palinkiewicz <pogromca.ap@gmail.com>
2023-11-08 08:44:48 +0100

    Add error messages

--- lekcja3/app/Http/Controllers/UserRegistrationController.php
+++ lekcja3/app/Http/Controllers/UserRegistrationController.php
@@ -11,6 +11,12 @@ class UserRegistrationController extends Controller
             'first_name' => 'required | min:5 | max:32',
             'last_name' => 'required | min:3 | max:32',
             'email' => 'required | email | min:2 | max:128'
+        ],
+        [
+            'first_name.required' => 'Pole :attribute jest wymagane',
+            'first_name.min' => 'Pole :attribute musi mieć minimalnie :min znaków',
+            'first_name.max' => 'Pole :attribute musi mieć maksymalnie :max znaków',
+            // etc
         ]);
         return view('show_registered_user', $req->all());
     }
--- lekcja3/resources/views/forms/userregister.blade.php
+++ lekcja3/resources/views/forms/userregister.blade.php
@@ -6,6 +6,11 @@
     <title>Register</title>
 </head>
 <body>
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
     <form action="register_user" method="get">
         First name: <input type="text" name="first_name"><br>
         Last name: <input type="text" name="last_name"><br>
