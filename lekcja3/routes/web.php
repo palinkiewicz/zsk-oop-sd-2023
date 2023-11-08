0539d4a66b71f0b4d7f353a9c94741bf9b5360b8
palinkiewicz <pogromca.ap@gmail.com>
2023-11-08 08:22:00 +0100

    Add user registration route

--- lekcja3/routes/web.php
+++ lekcja3/routes/web.php
@@ -33,3 +33,5 @@ Route::get('show_array', [ArrayController::class, 'show']);
 Route::view('user_form', 'forms.user');
 
 Route::get('submit_user', [\App\Http\Controllers\UserFormController::class, 'ShowUser']);
+
+Route::view('user_register', 'forms.userregister');
