a421927b22d7d0f51e270a296c5c5f796e1d7a88
palinkiewicz <pogromca.ap@gmail.com>
2023-11-08 08:19:00 +0100

    Add user registration form

---
+++ lekcja3/resources/views/forms/userregister.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+<body>
+    <form action="register_user" method="get">
+        First name: <input type="text" name="first_name"><br>
+        Last name: <input type="text" name="last_name"><br>
+        Email: <input type="email" name="email"><br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
