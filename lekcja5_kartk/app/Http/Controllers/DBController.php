2127bdd2ac4309d0ea83ce757a5140dd2cdd58d2
palinkiewicz <pogromca.ap@gmail.com>
2023-11-22 09:26:31 +0100

    Add user insertion in DBController

--- lekcja5_kartk/app/Http/Controllers/DBController.php
+++ lekcja5_kartk/app/Http/Controllers/DBController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DBController extends Controller
 {
@@ -12,5 +13,15 @@ class DBController extends Controller
             ->select('name', 'email')
             ->where('name', 'Jan')
             ->get();
+    }
+
+    function addSampleUser() {
+        DB::table('users')
+            ->insert([
+                'name' => 'Krystyna',
+                'email' => 'krystyna@o2.pl',
+                'password' => Hash::make('krystyna123')
+            ]);
+        return "Dodano.";
     }
 }
