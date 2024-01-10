9be494688fce815c85d939f5f1a5f328b47f4de6
palinkiewicz <pogromca.ap@gmail.com>
2024-01-10 07:55:04 +0100

    Create lekcja1_Lambda csharp project

---
+++ csharp/lekcja1_Lambda/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace lekcja1_Lambda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Napisz program, który wczytuje z klawiatury liczbę n, a następnie n imion i wieków osób.
+                1) Użyj tablicy, aby przechować wczytane imiona i wiek osób.
+                2) Użyj listy, aby przechować tylko te imiona, które zaczynają się na literę A.
+                3) Użyj słownika, aby przechować pary (imie, wiek) dla wszystkich osób, których wiek jest większy niż 18 lat.
+                4) Wypisz na ekranie zawartość tablicy, listy i słownika.
+            */
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
