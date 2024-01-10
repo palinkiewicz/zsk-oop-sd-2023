5f460df13b10a0106da0758c8cad7cb3e88e72fa
palinkiewicz <pogromca.ap@gmail.com>
2024-01-10 08:10:27 +0100

    Move the helper functions out of the main function

--- csharp/lekcja1_Lambda/Program.cs
+++ csharp/lekcja1_Lambda/Program.cs
@@ -10,48 +10,48 @@
                 3) Użyj słownika, aby przechować pary (imie, wiek) dla wszystkich osób, których wiek jest większy niż 18 lat.
                 4) Wypisz na ekranie zawartość tablicy, listy i słownika.
             */
+        }
 
-            static string ReadString(string prompt)
+        static string ReadString(string prompt)
+        {
+            string input = "";
+
+            do
             {
-                string input = "";
+                Console.Write("{0}: ", prompt);
+                input = Console.ReadLine() ?? "";
 
-                do
+                if (string.IsNullOrEmpty(input))
                 {
-                    Console.Write("{0}: ", prompt);
-                    input = Console.ReadLine() ?? "";
+                    Console.WriteLine("Podaj prawidłowy ciąg znaków.");
+                }
+            } while (string.IsNullOrEmpty(input));
 
-                    if (string.IsNullOrEmpty(input))
-                    {
-                        Console.WriteLine("Podaj prawidłowy ciąg znaków.");
-                    }
-                } while (string.IsNullOrEmpty(input));
+            return input;
+        }
 
-                return input;
-            }
+        static int ReadInt(string prompt, int lowest, int highest)
+        {
+            int input;
+            bool valid;
 
-            static int ReadInt(string prompt, int lowest, int highest)
+            do
             {
-                int input;
-                bool valid;
+                Console.Write("{0}: ", prompt);
+                valid = int.TryParse(Console.ReadLine(), out input);
 
-                do
+                if (!valid)
                 {
-                    Console.Write("{0}: ", prompt);
-                    valid = int.TryParse(Console.ReadLine(), out input);
+                    Console.WriteLine("Podaj prawidłową liczbę całkowitą.");
+                }
+                else if (input < lowest || input > highest)
+                {
+                    valid = false;
+                    Console.WriteLine("Liczba musi być w zakresie od {0} do {1}", lowest, highest);
+                }
+            } while (!valid);
 
-                    if (!valid)
-                    {
-                        Console.WriteLine("Podaj prawidłową liczbę całkowitą.");
-                    }
-                    else if (input < lowest || input > highest)
-                    {
-                        valid = false;
-                        Console.WriteLine("Liczba musi być w zakresie od {0} do {1}", lowest, highest);
-                    }
-                } while (!valid);
-
-                return input;
-            }
+            return input;
         }
     }
 }
