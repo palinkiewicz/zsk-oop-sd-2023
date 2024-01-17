c2d3f9c0b13155b35f418fce5d118111d7cbf5c8
palinkiewicz <pogromca.ap@gmail.com>
2024-01-17 08:03:59 +0100

    Add a new dotnet project

---
+++ csharp/lekcja2_Dziedziczenie/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace lekcja2_Dziedziczenie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
