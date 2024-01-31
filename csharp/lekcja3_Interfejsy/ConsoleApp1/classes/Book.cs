c7718675ee4927980af32c9743fdbbedd73ac044
palinkiewicz <pogromca.ap@gmail.com>
2024-01-31 08:45:58 +0100

    Create Book class

---
+++ csharp/lekcja3_Interfejsy/ConsoleApp1/classes/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.classes
+{
+    internal class Book : IComparable<Book>
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public ushort Year { get; set; }
+        public float Price {  get; set; }
+
+        public Book(string title, string author, ushort year, float price)
+        {
+            Title = title;
+            Author = author;
+            Year = year;
+            Price = price;
+        }
+
+        public int CompareTo(Book? other)
+        {
+            if (other == null) return 1;
+            return Price.CompareTo(other.Price);
+        }
+    }
+}
