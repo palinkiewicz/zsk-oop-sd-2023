0d8b35c1d9e48327418d24ee05dfe7830cfca9c8
palinkiewicz <pogromca.ap@gmail.com>
2024-02-07 09:06:38 +0100

    Create lesson 4 csharp project

---
+++ csharp/lekcja4_Delegaty/lekcja4_Delegaty/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace lekcja4_Delegaty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
