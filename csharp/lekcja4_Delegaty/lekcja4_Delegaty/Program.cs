8383b13d1ee71c5e879774e14724078e5dd92b38
palinkiewicz <pogromca.ap@gmail.com>
2024-02-07 09:13:06 +0100

    Create delegate and math operation methods

--- csharp/lekcja4_Delegaty/lekcja4_Delegaty/Program.cs
+++ csharp/lekcja4_Delegaty/lekcja4_Delegaty/Program.cs
@@ -2,6 +2,28 @@
 {
     internal class Program
     {
+        public delegate int Operation(int x, int y);
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Subtract(int x, int y) 
+        {
+            return x - y;
+        }
+
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        public static int Divide(int x, int y)
+        {
+            return x / y;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
