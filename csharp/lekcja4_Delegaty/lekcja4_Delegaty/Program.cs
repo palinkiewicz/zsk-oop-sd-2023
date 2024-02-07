f2b848574893fc236bd47b96e6527321d38f0d87
palinkiewicz <pogromca.ap@gmail.com>
2024-02-07 09:14:01 +0100

    Add task information

--- csharp/lekcja4_Delegaty/lekcja4_Delegaty/Program.cs
+++ csharp/lekcja4_Delegaty/lekcja4_Delegaty/Program.cs
@@ -1,4 +1,44 @@
-﻿namespace lekcja4_Delegaty
+﻿/*
+Zadania - delegaty
+
+● Napisz program w języku C#, który używa delegatów do wykonania
+operacji arytmetycznych na dwóch liczbach podanych przez
+użytkownika.
+
+● Zadeklaruj typ delegata, który przyjmuje i zwraca int, i nazwij go
+Operation.
+
+● Zdefiniuj cztery metody statyczne, które realizują operacje dodawania,
+odejmowania, mnożenia i dzielenia dwóch liczb, i nazwij je odpowiednio
+Add, Subtract, Multiply i Divide.
+
+● Zdefiniuj metodę statyczną, która wyświetla wynik operacji na liczbach, i
+nazwij ją DisplayResult. Metoda ta powinna przyjmować jako argumenty
+instancję delegata Operation, dwie liczby i wywoływać metodę związaną
+z delegatem na tych liczbach. Metoda ta powinna również obsługiwać
+wyjątek DivideByZeroException, jeśli wystąpi, i wyświetlać odpowiedni
+komunikat o błędzie.
+
+● Zdefiniuj funkcję, która pobiera liczbę całkowitą nieujemną od
+użytkownika, i nazwij ją GetIntFromUser. Funkcja ta powinna wyświetlać
+podpowiedź dla użytkownika, pobierać dane od użytkownika,
+sprawdzać, czy dane są poprawne i nieujemne, i zwracać liczbę. Jeśli
+dane są nieprawidłowe, funkcja powinna wyświetlać komunikat o
+błędzie i prosić użytkownika o ponowne wprowadzenie danych.
+
+● W metodzie Main, pobierz dwie liczby od użytkownika za pomocą
+funkcji GetIntFromUser i zapisz je w zmiennych a i b.
+
+● Utwórz cztery instancje delegata Operation i przypisz im metody Add,
+Subtract, Multiply i Divide.
+
+● Wywołaj metodę DisplayResult dla każdej instancji delegata i przekaż jej
+zmienne a i b jako argumenty.
+
+● Przetestuj swój program dla różnych danych wejściowych i sprawdź, czy
+działa poprawnie.
+*/
+namespace lekcja4_Delegaty
 {
     internal class Program
     {
