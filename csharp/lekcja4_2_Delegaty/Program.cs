ff0857188c5c98bf54f41b605548a29192c77af2
palinkiewicz <pogromca.ap@gmail.com>
2024-02-28 08:43:46 +0100

    Add some functions and a delegate

--- csharp/lekcja4_2_Delegaty/Program.cs
+++ csharp/lekcja4_2_Delegaty/Program.cs
@@ -1,7 +1,59 @@
 ﻿namespace _4_2_Delegaty
 {
+    delegate bool Logic(bool a, bool b);
+
     internal class Program
     {
+        static bool And(bool a, bool b)
+        {
+            return a && b;
+        }
+
+        static bool Or(bool a, bool b)
+        {
+            return a || b;
+        }
+
+        static bool Xor(bool a, bool b)
+        {
+            return a ^ b;
+        }
+
+        static bool Not(bool a, bool b)
+        {
+            return !a;
+        }
+
+        static void DisplayResult(Logic logic, bool a, bool b)
+        {
+            try
+            {
+                bool result = logic(a, b);
+                Console.WriteLine($"Rezultat {logic.Method.Name} {a} {b} wynosi {result}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static bool GetBoolFromUser() {
+            while (true)
+            {
+                Console.Write("Wprowadź true lub false: ");
+                bool value;
+
+                if (bool.TryParse(Console.ReadLine(), out value))
+                {
+                    return value;
+                }
+                else
+                {
+                    Console.WriteLine("Nieprawidłowe dane.");
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
