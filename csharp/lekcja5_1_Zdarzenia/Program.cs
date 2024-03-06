1c72460d6cb18264a2162f92231827f55ef85ae9
palinkiewicz <pogromca.ap@gmail.com>
2024-03-06 07:29:59 +0100

    Create new lesson project

---
+++ csharp/lekcja5_1_Zdarzenia/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace lekcja5_1_Zdarzenia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
