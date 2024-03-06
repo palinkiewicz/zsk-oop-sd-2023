0b88e75948979b6ce71a15935f8f1d36632b3777
palinkiewicz <pogromca.ap@gmail.com>
2024-03-06 07:35:46 +0100

    Create MessageHandler delegate

--- csharp/lekcja5_1_Zdarzenia/Program.cs
+++ csharp/lekcja5_1_Zdarzenia/Program.cs
@@ -1,5 +1,27 @@
-﻿namespace lekcja5_1_Zdarzenia
+﻿/*
+o Zdefiniuj delegat MessageHandler, który przyjmuje string i zwraca void.
+o Zdefiniuj klasę Publisher z polem MessageEvent typu MessageHandler,
+zadeklarowanym jako event.
+o Zdefiniuj metodę SendMessage w klasie Publisher, która przyjmuje string i zwraca
+void. Metoda ta ma wywoływać zdarzenie MessageEvent, jeśli ma subskrybentów.
+*/
+
+/*
+o Zdefiniuj klasę Subscriber z metodą OnMessageReceived, która przyjmuje string i
+zwraca void. Metoda ta ma wypisywać na konsolę wiadomość otrzymaną od
+wydawcy.
+o Zdefiniuj klasę Program z metodą Main, która jest punktem wejścia programu.
+o W metodzie Main utwórz obiekty klasy Publisher i klasy Subscriber.
+o Zasubskrybuj zdarzenie MessageEvent za pomocą metody OnMessageReceived.
+o Wywołaj metodę SendMessage kilka razy z różnymi parametrami i sprawdź, czy
+subskrybent otrzymuje wiadomości.
+o Anuluj subskrypcję zdarzenia i wywołaj metodę SendMessage ponownie. Sprawdź,
+czy subskrybent nadal otrzymuje wiadomości
+*/
+namespace lekcja5_1_Zdarzenia
 {
+    public delegate void MessageHandler(string message);
+
     internal class Program
     {
         static void Main(string[] args)
